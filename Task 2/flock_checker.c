6e38344c9539ac2b4ca04b5d09354b3b1d374bae
Alexander Chirkin <chirkin2001@list.ru>
2023-04-16 14:42:32 +0500

    task 2. file lock (README, fixing checker, logging, stats)

--- Task 2/flock_checker.c
+++ Task 2/flock_checker.c
@@ -5,46 +5,131 @@
 #include <string.h>
 #include <math.h>
 #include <signal.h>
+#include <ctype.h>
+#include <errno.h>
+
+#define STAT_TEMPLATE "Successful locks number for pid %d: %d\n"
+#define LOCK_OPEN_ERROR_MSG "An error occurred while opening lock file %s\n"
+#define LOCK_WRITE_ERROR_MSG "An error occurred while writing to lock file %s\n"
+#define LOCK_READ_ERROR_MSG "An error occurred while reading from lock file %s\n"
+#define LOCK_BY_ANOTHER_PROCESS "File %s has been already locked by another process with pid %s (current pid=%s)\n"
+#define FILE_OPEN_ERROR_MSG "An error occurred while opening file %s\n"
+#define FILE_WRITE_ERROR_MSG "An error occurred while writing to file %s\n"
+#define SIGINT_ERROR "An error with code %d occurred while handling SIGINT\n"
 
 static volatile int running = 1;
 static volatile int pid;
 static volatile int successful_locks = 0;
+static volatile int locked = 0;
+static volatile int write_stats = 0;
 
-void sigintHandler(int sig_num){
+void sigintHandler(int sig_num) {
+    errno = 0;
     running = 0;
-    int stat_fd = open("stats.txt", O_CREAT | O_WRONLY | O_APPEND, 0600);
-    int locks_num_buffer_len = (int) ((ceil(log10(pid)) + ceil(log10(successful_locks)) + 36) * sizeof(char));
-    char locks_num_buffer[locks_num_buffer_len];
-    sprintf(locks_num_buffer, "Successful locks number for pid %d: %d\n", pid, successful_locks);
-    write(stat_fd, locks_num_buffer, locks_num_buffer_len);
-    close(stat_fd);
+    if (write_stats) {
+        int stat_fd = open("stats.txt", O_CREAT | O_WRONLY | O_APPEND, 0600);
+        int locks_num_buffer_len = (int) ((ceil(log10(pid)) + ceil(log10(successful_locks)) + 35) * sizeof(char));
+        char locks_num_buffer[locks_num_buffer_len];
+        sprintf(locks_num_buffer, STAT_TEMPLATE, pid, successful_locks + locked);
+        write(stat_fd, locks_num_buffer, locks_num_buffer_len);
+        close(stat_fd);
+        if (opterr > 0) {
+            fprintf(stderr, SIGINT_ERROR, errno);
+        }
+    }
+}
+
+int read_args(int argc, char** argv) {
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt (argc, argv, "s")) != -1)
+        switch (c)
+        {
+            case 's':
+                write_stats = 1;
+                break;
+            case '?':
+                if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,"Unknown option character `\\x%x'.\n", optopt);
+                return 1;
+            default:
+                abort ();
+        }
+}
+
+int handle_file_exception(char* template, char* arg, int fd, int lock_fd) {
+    fprintf(stderr, template, arg);
+    close(lock_fd);
+    close(fd);
+    return 1;
 }
 
 int main(int argc, char** argv) {
     signal(SIGINT, sigintHandler);
-    char* file_name = argv[1];
+
+    read_args(argc, argv);
+
+    char* file_name = argv[optind];
     char* lck_file_name = (char*) malloc((strlen(file_name) + 4) * sizeof(char));
     sprintf(lck_file_name, "%s.lck", file_name);
-    int lock_fd, fd;
+
     pid = getpid();
-    int pid_len = (int) ((ceil(log10(pid))) * sizeof(char));
+    int pid_len = (int) ((ceil(log10(pid)) + 1) * sizeof(char));
+    char *pid_bytes = (char*) malloc(pid_len);
+    sprintf(pid_bytes, "%d\n", pid);
+
+    int lock_fd, fd, written, read_bytes;
+    char read_buffer[pid_len];
+
     while(running) {
         lock_fd = -1;
         while (lock_fd == -1) {
-            lock_fd = open(lck_file_name, O_CREAT | O_EXCL | O_WRONLY, 0200);
+            lock_fd = open(lck_file_name, O_CREAT | O_EXCL | O_WRONLY, 0600);
+            sleep(0.1);
         }
 
-        char *pid_bytes = (char*) malloc(pid_len);
-        sprintf(pid_bytes, "%d", pid);
-        write(lock_fd, pid_bytes, pid_len);
-        close(lock_fd);
+        written = write(lock_fd, pid_bytes, pid_len);
+        if (written == -1)
+            return handle_file_exception(LOCK_WRITE_ERROR_MSG, lck_file_name, fd, lock_fd);
 
-        fd = open(file_name, O_RDWR);
-        write(fd, pid_bytes, pid_len);
+        close(lock_fd);
+        locked = 1;
+
+        fd = open(file_name, O_WRONLY | O_APPEND);
+        if (fd == -1)
+            return handle_file_exception(FILE_OPEN_ERROR_MSG, file_name, fd, lock_fd);
+
+        written = write(fd, pid_bytes, pid_len);
+        if (written == -1)
+            return handle_file_exception(FILE_WRITE_ERROR_MSG, file_name, fd, lock_fd);
+
         close(fd);
 
         sleep(1);
+
+        lock_fd = open(lck_file_name, O_RDONLY);
+        if (lock_fd == -1)
+            return handle_file_exception(LOCK_OPEN_ERROR_MSG, lck_file_name, fd, lock_fd);
+
+        read_bytes = read(lock_fd, read_buffer, pid_len);
+        if (read_bytes == -1)
+            return handle_file_exception(LOCK_READ_ERROR_MSG, lck_file_name, fd, lock_fd);
+
+        if (strcmp(read_buffer, pid_bytes)) {
+            fprintf(stderr, LOCK_BY_ANOTHER_PROCESS, file_name, read_buffer, pid_bytes);
+            return 1;
+        }
+
+        close(lock_fd);
+
         remove(lck_file_name);
+        locked = 0;
         successful_locks++;
     }
+
+    return errno;
 }
--- Task 2/runme.sh
+++ Task 2/runme.sh
@@ -2,22 +2,27 @@ log_to_result() {
   printf '%s %s\n' "$(date +'%d.%m.%y %H:%M:%S.%N')" "$1" >> result.txt
 }
 
-rm stats.txt result.txt
+rm -f stats.txt result.txt myfile*
 touch myfile
 
 log_to_result 'Compiling file lock checker'
 make && log_to_result 'File lock checker compiled successfully!'
 chmod 700 flock_checker
 
+log_to_result 'Checking that lock fails after deleting .lck file'
 ( ./flock_checker myfile ) &
 ( ./flock_checker myfile ) &
-rm myfile.lck
+rm -f myfile.lck
 sleep 2
 killall -SIGINT flock_checker
+log_to_result 'Printing contested file content'
+cat myfile >> result.txt
 
+log_to_result 'Checking lock for 10 parallel tasks'
 for _ in {1..10}
 do
- ( ./flock_checker myfile ) &
+ ( ./flock_checker -s myfile ) &
 done
 sleep 300
-killall -SIGINT flock_checker
+log_to_result 'Killing tasks with SIGINT'
+killall -SIGINT flock_checker && log_to_result 'Killed'
